abecd7d5bbede92f49f91564603140a073e38d69
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-03-11 08:21:26 -0600

    se creo el modelo detalleproveedor

---
+++ JDMR20241103/Models/DetalleProveedor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace JDMR20241103.Models
+{
+    public class DetalleProveedor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProveedorId { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Solo se permiten letras")]
+        [StringLength(100)]
+        public string Calle { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Solo se permiten letras")]
+        [StringLength(100)]
+        public string Ciudad { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Solo se permiten letras")]
+        [StringLength(100)]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Solo se permiten letras")]
+        [StringLength(100)]
+        public string Pais { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Solo se permiten letras")]
+        [StringLength(100)]
+        public string CodigoPostal { get; set; } 
+
+        public virtual Proveedor Proveedor { get; set; } = null!;
+    }
+}
