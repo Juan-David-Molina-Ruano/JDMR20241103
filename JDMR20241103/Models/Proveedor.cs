5f60529f1b616d6024788ecf6fd4ce0333dc4723
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-03-11 08:20:13 -0600

    se creo el modelo proveedor

---
+++ JDMR20241103/Models/Proveedor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace JDMR20241103.Models
+{
+    public class Proveedor
+    {
+        public Proveedor()
+        {
+            DetalleProveedores = new List<DetalleProveedor>();
+        }
+
+
+        [Key]
+        public int Id { get; set; }
+
+        public virtual IList<DetalleProveedor> DetalleProveedores { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Solo se permiten letras")]
+        [StringLength(60, ErrorMessage = "El campo {0} debe tener una longitud máxima de {1} caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Solo se permiten números")]
+        [StringLength(10, ErrorMessage = "El campo {0} debe tener una longitud máxima de {1} caracteres")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Solo se permiten letras")]
+        [StringLength(200, ErrorMessage = "El campo {0} debe tener una longitud máxima de {1} caracteres")]
+        public string Descripcion { get; set; }
+
+    }
+}
