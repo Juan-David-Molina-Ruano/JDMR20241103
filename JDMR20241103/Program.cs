0ce27fa428b77165d5d6665eda73570bfd8a6ebb
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-03-11 08:36:47 -0600

    se inyecto la dependecia de servicios para conectarse a la base de datos

--- JDMR20241103/Program.cs
+++ JDMR20241103/Program.cs
@@ -1,7 +1,13 @@
+using JDMR20241103.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
 
 var app = builder.Build();
 
