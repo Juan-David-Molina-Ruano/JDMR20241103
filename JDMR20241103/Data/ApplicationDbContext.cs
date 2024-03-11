eddf3fcfd02209b8e1886ca9a6c65a279743e077
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-03-11 08:36:13 -0600

    Se creo el contexto de la aplicacion

---
+++ JDMR20241103/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JDMR20241103.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace JDMR20241103.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<DetalleProveedor> DetalleProveedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Proveedor>()
+                .HasMany(p => p.DetalleProveedores)
+                .WithOne(d => d.Proveedor)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+
+    }
+}
